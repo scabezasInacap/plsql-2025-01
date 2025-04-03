e628cf265c21518d2269f208105260635d7694b2
scabezasInacap <sebastian.cabezas07@inacapmail.cl>
2025-04-03 17:53:59 -0300

    Create antes-eva1-03-vista.sql

---
+++ antes-eva1-03-vista.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE VIEW v_vista1 AS
+SELECT 
+    CONCAT('123-',empleado_id) as "RUT",
+    primer_nombre || ' ' || apellido as "NOMBRE_COMPLETO",
+    sueldo,
+    -- e.departamento_id as DEPARTMENT_ID,
+    d.departamento_nombre as DEPARTMENT_NAME
+FROM 
+    empleado e JOIN departamento d ON (e.departamento_id = d.departamento_id)
+ORDER BY 
+    empleado_id ASC;
